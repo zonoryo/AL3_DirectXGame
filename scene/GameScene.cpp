9d359db5f207ad504c99fac084a724f9e0668f75
zonoryo <k022g1035@g.neec.ac.jp>
2023-04-24 14:27:13 +0900

    01_02_確認課題

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -1,11 +1,15 @@
 #include "GameScene.h"
 #include "TextureManager.h"
 #include <cassert>
-
+#include "ImGuiManager.h"
+#include "PrimitiveDrawer.h"
+#include "AxisIndicator.h"
 GameScene::GameScene() {}
 
 GameScene::~GameScene() { 
 	delete sprite_;
+	delete model_;
+	delete debugCamera_;
 }
 
 void GameScene::Initialize() {
@@ -16,13 +20,25 @@ void GameScene::Initialize() {
 
 	textureHandle_ = TextureManager::Load("sample.png");
 	sprite_ = Sprite::Create(textureHandle_, {100, 50});
-	
+	model_ = Model::Create();
+	WorldTransform_.Initialize();
+	ViewProjection_.Initialize();
+	PrimitiveDrawer::GetInstance()->SetViewProjection(&ViewProjection_);
+	debugCamera_ = new DebugCamera(1280, 720);
+	AxisIndicator::GetInstance()->SetVisible(true);
+	AxisIndicator::GetInstance()->SetTargetViewProjection(&debugCamera_->GetViewProjection());
 }
 
-void GameScene::Update() {
-	//スプライト
+void GameScene::Update() { 
+	ImGui::Begin("Debug1");
+	ImGui::InputFloat3("InputFloat3", inputFloat3);
+	ImGui::SliderFloat3("SliderFloat3", inputFloat3,0.0f,1.0f);
+	ImGui::Text("Kamata Tarou %d.%d.%d", 2050, 12, 31); 
+	ImGui::ShowDemoWindow();
+	ImGui::End();
+	debugCamera_->Update();
 	Vector2 position = sprite_->GetPosition();
-	position.x += 1.0f;
+	position.x += 2.0f;
 	position.y += 1.0f;
 	sprite_->SetPosition(position);
 }
@@ -53,7 +69,9 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
-
+	model_->Draw(WorldTransform_, debugCamera_->GetViewProjection(), textureHandle_);
+	model_->Draw(WorldTransform_, ViewProjection_, textureHandle_);
+	
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
 #pragma endregion
@@ -65,9 +83,10 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
-	sprite_->Draw();
+	
 	// スプライト描画後処理
+	sprite_->Draw();
 	Sprite::PostDraw();
-
-#pragma endregion
+	PrimitiveDrawer::GetInstance()->DrawLine3d({0, 0, 0}, {0, 10, 0}, {1.0f, 0.0f, 0.0f, 1.0f});
 }
+
--- scene/GameScene.h
+++ scene/GameScene.h
@@ -8,6 +8,7 @@
 #include "Sprite.h"
 #include "ViewProjection.h"
 #include "WorldTransform.h"
+#include "DebugCamera.h"
 
 /// <summary>
 /// ゲームシーン
@@ -41,6 +42,7 @@ public: // メンバ関数
 	void Draw();
 
 private: // メンバ変数
+	Sprite* sprite_ = nullptr;
 	DirectXCommon* dxCommon_ = nullptr;
 	Input* input_ = nullptr;
 	Audio* audio_ = nullptr;
@@ -53,5 +55,11 @@ private: // メンバ変数
 	uint32_t textureHandle_ = 0;
 
 	//スプライト
-	Sprite* sprite_ = nullptr;
+	Model* model_ = nullptr;
+
+	WorldTransform WorldTransform_;
+	ViewProjection ViewProjection_;
+	float inputFloat3[3] = {0, 0, 0};
+
+	DebugCamera* debugCamera_ = nullptr;
 };
